3ee8a216cf8fda49f6b19a1fbae86ff79824ceee
Daniel Silva <diielsilvasousa@gmail.com>
2024-11-08 16:04:42 -0300

    chore: adding Readme

---
+++ README.md
@@ -0,0 +1,6 @@
+## Customers API
+This is a simple repository that stores a CRUD API that runs with .NET 8.0
+
+### How to Run?
+1 - You will need a MySQL Container
+2 - You will need to run the Migrations 
--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71a7bb7454adacb213901dbe56807ca8805fdf96")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
